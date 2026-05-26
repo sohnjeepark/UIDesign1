d9cfea885efd6c92a468bc8df4c4f442f1d65e6b
Sohnjee Park <sohnjeepark@snu.ac.kr>
2026-05-26 15:59:03 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
+const type = document.querySelector(".interactive-type");
 
+document.addEventListener("mousemove", function(event) {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  const xPercent = x / windowWidth;
+  const yPercent = y / windowHeight;
+
+  const letterSpace = xPercent * 30;
+  const scale = 1 + yPercent * 1.5;
+
+  type.style.letterSpacing = letterSpace + "px";
+  type.style.transform = `scale(${scale})`;
+});
